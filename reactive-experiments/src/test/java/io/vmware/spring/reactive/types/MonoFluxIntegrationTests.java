887dc53d7cd5156803fe492716695b7d69901ae6
John Blum <jblum0@gmail.com>
2023-10-12 13:05:48 -0700

    Add additional test cases with Mono using map(..) and then(..) functions / operators.

--- reactive-experiments/src/test/java/io/vmware/spring/reactive/types/MonoFluxIntegrationTests.java
+++ reactive-experiments/src/test/java/io/vmware/spring/reactive/types/MonoFluxIntegrationTests.java
@@ -17,17 +17,35 @@ package io.vmware.spring.reactive.types;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 import org.junit.jupiter.api.Test;
 
+import org.cp.elements.lang.ObjectUtils;
+import org.cp.elements.lang.ThrowableOperation;
+
+import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -84,5 +102,98 @@ class MonoFluxIntegrationTests {
 		assertThat(exception).hasCauseInstanceOf(NullPointerException.class);
 		assertThat(exception.getCause()).hasMessageContaining("iterator returned a null value");
 		assertThat(exception.getCause()).hasNoCause();
+	}
+
+	@Test
+	@SuppressWarnings("all")
+	void monoFromSupplierOnSubscriptionWorksAsExpected() throws InterruptedException{
+
+		AtomicInteger count = new AtomicInteger(0);
+		CountDownLatch latch = new CountDownLatch(1);
+
+		Supplier<Integer> blockingIntegerSupplier = () -> {
+
+			System.out.println("Waiting on CountDownLatch...");
+
+			ObjectUtils.doOperationSafely(ThrowableOperation.fromVoidReturning(args -> latch.await()));
+
+			System.out.println("CountDownLatch.await() returned...");
+
+			return count.incrementAndGet();
+		};
+
+		/*
+		Disposable disposable = Mono.fromSupplier(blockingIntegerSupplier)
+			.subscribe(System.out::println);
+		*/
+
+		Disposable disposable = Mono.fromFuture(CompletableFuture.supplyAsync(blockingIntegerSupplier))
+			.subscribe(System.out::println);
+
+		assertThat(disposable).isNotNull();
+		assertThat(count.get()).isZero();
+		assertThat(latch.getCount()).isOne();
+
+		System.out.println("YOU ARE HERE");
+
+		latch.countDown();
+
+		while (!disposable.isDisposed()) {
+			synchronized (disposable) {
+				TimeUnit.MILLISECONDS.timedWait(disposable, 500);
+			}
+		}
+
+		assertThat(count.get()).isOne();
+		assertThat(latch.getCount()).isZero();
+	}
+
+	@Test
+	@SuppressWarnings("unchecked")
+	void monoEmittingValueWithMapWorksAsExpected() {
+
+		Function<String, String> mockFunction = mock(Function.class);
+
+		doAnswer(invocationOnMock -> {
+			String argument = invocationOnMock.getArgument(0);
+			return String.valueOf(argument).toUpperCase();
+		}).when(mockFunction).apply(any());
+
+		assertThat(Mono.fromSupplier(() -> "test").map(mockFunction).block()).isEqualTo("TEST");
+
+		verify(mockFunction, times(1)).apply(eq("test"));
+		verifyNoMoreInteractions(mockFunction);
+	}
+
+	@Test
+	@SuppressWarnings("unchecked")
+	void monoEmittingNullWithMapWorksAsExpected() {
+
+		Function<Object, Object> mockFunction = mock(Function.class);
+
+		doAnswer(invocationOnMock ->
+			ObjectUtils.requireObject(invocationOnMock.getArgument(0), "Argument is required")
+		).when(mockFunction).apply(any());
+
+		assertThat(Mono.fromSupplier(() -> null).map(mockFunction).block()).isNull();
+
+		verify(mockFunction, never()).apply(any());
+		verifyNoInteractions(mockFunction);
+	}
+
+	@Test
+	void monoEmittingValueWithThenWorksAsExpected() {
+
+		Mono<String> thenMono = Mono.fromSupplier(() -> "mock");
+
+		assertThat(Mono.fromSupplier(() -> "test").then(thenMono).block()).isEqualTo("mock");
+	}
+
+	@Test
+	void monoEmittingNullWithThenWorksAsExpected() {
+
+		Mono<String> thenMono = Mono.fromSupplier(() -> "nonnull");
+
+		assertThat(Mono.fromSupplier(() -> null).then(thenMono).block()).isEqualTo("nonnull");
 	}
 }
